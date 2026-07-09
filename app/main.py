e7764abef2ad980cd420a7a5b37f576edb1570ec
Syed Razvi <razvisyed2002@gmail.com>
2026-07-09 21:51:41 +0530

    Modified delete and update method using sqlalchemy

--- app/main.py
+++ app/main.py
@@ -95,28 +95,31 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
-    deleted_post = cursor.fetchone()
+def delete_post(id: int, db: Session = Depends(get_db)):
+    #cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    #deleted_post = cursor.fetchone()
 
-    conn.commit()
-
-    if  deleted_post == None:
+    #conn.commit()
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if  post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id {id} does not exist')
-    
+    post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
 
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, (post.title, post.content, post.published, str(id)))
+    #cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,(post.title, post.content, post.published, str(id)))
 
-    updated_post = cursor.fetchone()
+    #updated_post = cursor.fetchone()
 
-    conn.commit()
-
-    if  update_post == None:
+    #conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if  post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id {id} does not exist')
-
-    return {'data' : updated_post}
+    post_query.update(updated_post.model_dump(), synchronize_session=False)
+    db.commit()
+    return {'data' : post_query.first()}
