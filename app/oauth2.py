717c665af60f8c61630b3acec849fca1119f8749
Syed Razvi <razvisyed2002@gmail.com>
2026-07-12 20:29:16 +0530

    verifying the user login

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,10 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schemas
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+
+oauth2_scheme = OAuth2PasswordBearer
 
 SECRET_KEY = "i52ub3243bnkj24b42ubofb42ou6tbobfno4bou26"
 ALGORITHM = "HS256"
@@ -8,9 +13,30 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data : dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp" : expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def verify_access_token(token : str, credentials_exception):
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id : str = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id = id)
+    except JWTError:
+        raise credentials_exception
+    
+    return token_data
+    
+
+def get_current_user(token : str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate" : "Bearer"})
+
+    return verify_access_token(token, credentials_exception)
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import Optional
 
 class Post(BaseModel):
     title: str
@@ -37,3 +38,12 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email : EmailStr
     password : str
+
+
+class Token(BaseModel):
+    access_token : str
+    token_type : str
+
+
+class TokenData(BaseModel):
+    id : Optional[str] = None
