e895ef14bffd7e6a716724d56053abc6b84a78f9
Syed Razvi <razvisyed2002@gmail.com>
2026-07-22 22:07:19 +0530

    clean up main.py file

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:rsadnan020626@localhost/fastAPI'
 
@@ -16,3 +19,15 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# while True:
+  #  try:
+   #     conn = psycopg2.connect(host = 'localhost', database = 'fastAPI', user = 'postgres', password = #'rsadnan020626', cursor_factory = RealDictCursor)
+    #    cursor = conn.cursor()
+     #   print("Database Connected")
+      #  break
+    #except Exception as error:
+     #   print("Connection to Database failed")
+      #  print("Error", error)
+       # time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,47 +1,12 @@
-from typing import Optional, List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from . import models, schemas, utils
-from .database import engine, SessionLocal, get_db
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-
-
-
-while True:
-    try:
-        conn = psycopg2.connect(host = 'localhost', database = 'fastAPI', user = 'postgres', password = 'rsadnan020626', cursor_factory = RealDictCursor)
-        cursor = conn.cursor()
-        print("Database Connected")
-        break
-    except Exception as error:
-        print("Connection to Database failed")
-        print("Error", error)
-        time.sleep(2)
-
-my_posts = [{"title" : "The Dark Knight", "content" : "Batman and the Joker", "id" : 1},
-            {"title" : "The Long Halloween", "content" : "Batman and Carmine Falcone", "id" : 2}]
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
 
 app.include_router(post.router)
 app.include_router(user.router)
