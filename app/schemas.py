68f7c747d490909cc70af703c1c0834e103c7b26
Syed Razvi <razvisyed2002@gmail.com>
2026-07-23 22:40:14 +0530

    implemented logic of adding and deleting votes

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
+from pydantic.types import conint
 
 class Post(BaseModel):
     title: str
@@ -53,3 +54,8 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id : Optional[int] = None
+
+
+class Vote(BaseModel):
+    post_id : int
+    dir : conint(le=1) # type: ignore
